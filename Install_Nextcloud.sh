0e06414fdd5a16e4e5cc34c7ad07c41dee9365fa
czebiniak <30630518+czebiniak@users.noreply.github.com>
2017-12-20 13:47:30 -0500

    Ver 1.1
    minor changes

--- Install_Nextcloud.sh
+++ Install_Nextcloud.sh
@@ -5,6 +5,9 @@ NCDB="ncdb"
 NCUSER="ncuser"
 NCUSERPASS="$(openssl rand -base64 12)"
 clear
+sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get install apache2 mariadb-server -y
+clear
 #----------------------------- User Input --------------------------------------------------
 echo "Please enter root user MySQL password... (Password type prompt is hidden)"
 read -s rootpasswd
@@ -14,10 +17,7 @@ read -e -p "Desired user name for NextCloud... default is [$NCUSER]: " -i "$NCUS
 read -e -p "Desired password for NextCloud user... default is [$NCUSERPASS]: " -i "$NCUSERPASS" NCUSERPASS
 #-------------------------------------------------------------------------------------------
 clear
-sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install php-gd php-json php-mysql php-curl php-intl php-mcrypt php-imagick php-zip php-dom php7.0-xml php-mbstring wget unzip -y
-sudo apt-get install apache2 mariadb-server -y
-sudo apt-get install phpmyadmin -y
 sudo cp php.ini /etc/php/7.0/apache2/
 sudo systemctl restart apache2
 clear
@@ -26,7 +26,7 @@ echo "Database name chosen is: $NCDB" &>> ./NextCloudLog.log
 echo "Database user chosen is: $NCUSER" &>> ./NextCloudLog.log
 echo "Database password chosen is: $NCUSERPASS" &>> ./NextCloudLog.log
 mysql -uroot -p${rootpasswd} -e "DROP DATABASE IF EXISTS ${NCDB};"
-mysql -uroot -p${rootpasswd} -e "DROP USER IF EXISTS ${NCUSER}@localhost ;"
+#mysql -uroot -p${rootpasswd} -e "DROP USER IF EXISTS ${NCUSER}@localhost;"
 mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
 mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${NCDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
 mysql -uroot -p${rootpasswd} -e "CREATE USER ${NCUSER}@localhost IDENTIFIED BY '${NCUSERPASS}';"
