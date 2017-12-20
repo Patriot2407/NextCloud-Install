dda4f8366c41f5a90a4c2dca53dc547e37de396e
czebiniak <30630518+czebiniak@users.noreply.github.com>
2017-12-20 11:56:07 -0500

    Ver 1.1
    mysql changes, many other changes

--- Install_Nextcloud.sh
+++ Install_Nextcloud.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #---- executable --------
 IPADD=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
+NCDB="ncdb"
+NCUSER="ncuser"
+NCUSERPASS="$(openssl rand -base64 12)"
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install php-gd php-json php-mysql php-curl php-intl php-mcrypt php-imagick php-zip php-dom php7.0-xml php-mbstring wget unzip -y
 sudo apt-get install apache2 mariadb-server -y
@@ -8,40 +11,25 @@ sudo apt-get install phpmyadmin -y
 sudo cp php.ini /etc/php/7.0/apache2/
 sudo systemctl restart apache2
 clear
-echo "Please run this snippet inside mysql after you have typed in the password highlight this snippet and right click, it will automatically paste and run inside this script"
-echo "Or just hit enter to make it later. It is not necessary to install NextCloud."
-echo ""
-echo "
-CREATE DATABASE ncdb;
-CREATE USER 'ncuser'@'localhost' IDENTIFIED BY 'ubuntu!!1234567';
-GRANT ALL PRIVILEGES ON ncdb.* TO 'ncuser'@'localhost' IDENTIFIED BY 'ubuntu!!1234567';
-FLUSH PRIVILEGES;
-\q
-"
-echo ""
-mysql -u root -p
+read -p "Desired databse name for NextCloud default is [$NCDB]: " NCDB
+read -p "Desired user name for NextCloud default is [$NCUSER]: " NCUSER
+read -p "Desired databse name for NextCloud default is [$NCUSERPASS]: " NCUSERPASS
+echo "Please enter root user MySQL password!"
+read rootpasswd
+mysql -uroot -p${rootpasswd} -e "CREATE DATABASE ${NCDB} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+mysql -uroot -p${rootpasswd} -e "CREATE USER ${NCUSER}@localhost IDENTIFIED BY '${NCUSERPASS}';"
+mysql -uroot -p${rootpasswd} -e "GRANT ALL PRIVILEGES ON ${NCDB}.* TO '${NCUSER}'@'localhost' IDENTIFIED BY '${NCUSERPASS}';"
+mysql -uroot -p${rootpasswd} -e "FLUSH PRIVILEGES;"
 wget https://download.nextcloud.com/server/releases/nextcloud-12.0.4.zip
 unzip nextcloud*
 sudo cp -r nextcloud/ /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/nextcloud/
-sudo echo "
-Alias /nextcloud "/var/www/html/nextcloud/"
-
-<Directory /var/www/html/nextcloud/>
-  Options +FollowSymlinks
-  AllowOverride All
-
- <IfModule mod_dav.c>
-  Dav off
- </IfModule>
-
- SetEnv HOME /var/www/html/nextcloud
- SetEnv HTTP_HOME /var/www/html/nextcloud
-
-</Directory>
-" >> /etc/apache2/sites-available/nextcloud.conf
+sudo rm -f /etc/apache2/sites-available/nextcloud.conf
+sudo cat nexcloud.conf >> /etc/apache2/sites-available/nextcloud.conf
 clear
+sudo chmod 755 /etc/apache2/sites-available/nextcloud.conf
 sudo ln -s /etc/apache2/sites-available/nextcloud.conf /etc/apache2/sites-enabled/nextcloud.conf
+sudo chmod -R 755 /var/www/html/nextcloud/
 sudo a2enmod rewrite
 sudo a2enmod headers
 sudo a2enmod env
